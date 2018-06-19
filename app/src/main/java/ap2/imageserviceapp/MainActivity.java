21dfc32e149edbe7d79892a6dec8e784afa0caf5
Gotesu <gotesuriel@gmail.com>
2018-06-19 12:28:48 +0300

    button link

--- app/src/main/java/ap2/imageserviceapp/MainActivity.java
+++ app/src/main/java/ap2/imageserviceapp/MainActivity.java
@@ -3,6 +3,8 @@ package ap2.imageserviceapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startService(View view) {
+        Toast.makeText(this,"You just Pushed the Start button",Toast.LENGTH_SHORT).show();
+    }
+
+    public void stopService(View view) {
+        Toast.makeText(this,"You just Pushed the Stop button",Toast.LENGTH_SHORT).show();
     }
 
 }
